5f3fd8d8616db2b1b00c5c00aebe6ea599845a7b
7AleGz <7alegz@gmail.com>
2023-11-02 16:35:38 -0300

    [ADD] Entity Edge changes

--- Entities/Edge.vb
+++ Entities/Edge.vb
@@ -1,7 +1,11 @@
-﻿Public Class Edge
+﻿Imports System.Net
+
+Public Class Edge
+    Public Shared Property MaxId As Integer = 0
     Public Property VertexTuple As Tuple(Of Integer, Integer)
 
     Public Sub New(originVertex As Integer, endVertex As Integer)
         VertexTuple = New Tuple(Of Integer, Integer)(originVertex, endVertex)
+        MaxId += 1
     End Sub
 End Class
